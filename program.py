7532aaa174b21962ddaaca891a84784ab5354328
komakul27 <divyakomakul@gmail.com>
2025-11-27 11:28:30 +0530

    Update program.py

--- program.py
+++ program.py
@@ -1,3 +1,11 @@
 print("welocme")
 
-#https://github.com/PhapaleSai/College_repo
+git clone <repo-url>
+cd practice_check/
+echo "i am sai" > sai.txt
+git add sai.txt
+git commit -m "the file is added"
+git checkout -b feature
+git push origin feature
+git checkout main
+git pull origin main
